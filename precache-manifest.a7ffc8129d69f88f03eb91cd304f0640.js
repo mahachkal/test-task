f72373e0f530071c7a17f5b58b748d464694b176
Vladimyr Oleynik <vladimyr.oleynik@vedidev.com>
2020-01-28 21:34:16 +0200

    Updates

---
+++ precache-manifest.a7ffc8129d69f88f03eb91cd304f0640.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b064df5fd75cc1045eec90c9d1a0af5c",
+    "url": "/test-task/index.html"
+  },
+  {
+    "revision": "eb511fc82ca31a0595e8",
+    "url": "/test-task/static/css/main.a2bd5671.chunk.css"
+  },
+  {
+    "revision": "a0a8af78d4058395b11d",
+    "url": "/test-task/static/js/2.ddd07f5d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/test-task/static/js/2.ddd07f5d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "eb511fc82ca31a0595e8",
+    "url": "/test-task/static/js/main.c7947495.chunk.js"
+  },
+  {
+    "revision": "7ec02d55516d584bf129",
+    "url": "/test-task/static/js/runtime-main.3f37f4d3.js"
+  }
+]);
